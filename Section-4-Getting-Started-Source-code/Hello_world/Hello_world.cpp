dd3c29b0746dbcab569d9a5ffa8adc22b9c9834f
VikrantWaje <viwa0175@colorado.edu>
2019-08-10 20:59:44 -0700

    Added solution for section 4 challenge

---
+++ Section-4-Getting-Started-Source-code/Hello_world/Hello_world.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+void main() {
+	int num = 0;
+	cout << "Enter favourite number between 1 and 100: ";
+	cin >> num;
+	cout << "Amazing!! That's my favourite number too" << endl;
+	cout << "No really " << num << " is my favourite number as well" << endl;
+}
